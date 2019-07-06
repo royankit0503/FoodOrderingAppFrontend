8a09aaa0ef4d373939a13bc849cc2859782d38aa
Kadavergu Bharath Kumar <kbharathkumar02@gmail.com>
2019-07-07 00:25:27 +0530

    Updated controller page

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Home from './home/Home';
+import Details from './details/Details';
+import Checkout from './checkout/Checkout';
+import Profile from './profile/Profile';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 class Controller extends Component {
@@ -14,8 +17,23 @@ class Controller extends Component {
             <Router>
                 <div className='main-container'>
 
-                    {/* route to home page */}
+                    {/* Routing to the Home page */}
                     <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
+
+                    {/* Routing to the Details page */}
+                    <Route path='/restaurant/:id' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
+
+                    {/* Routing to the Checkout page or redirect to the Home page based on the condition of customer-cart */}
+                    <Route path='/checkout' render={(props) => (
+                        sessionStorage.getItem('customer-cart') === null ? (
+                            <Redirect to='/' />
+                        ) : (
+                            <Checkout {...props} baseUrl={this.baseUrl} />
+                        )
+                    )} />
+
+                    {/* Routing to the Profile page */}
+                    <Route path='/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl} />} />
                 </div>
             </Router>
         )
