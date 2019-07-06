d9e6100a6a1423d46a289e05bf3542e3f2137b36
Somu <somuhere@gmail.com>
2019-07-06 19:35:32 +0530

    routing in the controller component added

---
+++ src/screens/Controller.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Home from './home/Home';
+import Details from './details/Details';
+import Checkout from './checkout/Checkout';
+import Profile from './profile/Profile';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+
+class Controller extends Component {
+
+    constructor() {
+        super();
+        this.baseUrl = 'http://localhost:8080/api/';
+    }
+
+    render() {
+        return (
+            <Router>
+                <div className='main-container'>
+
+                    {/* Routing to the Home page */}
+                    <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
+
+                    {/* Routing to the Details page */}
+                    <Route path='/restaurant/:id' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
+
+                    {/* Routing to the Checkout page or redirect to the Home page based on the condition of customer-cart */}
+                    <Route path='/checkout' render={(props) => (
+                        sessionStorage.getItem('customer-cart') === null ? (
+                            <Redirect to='/' />
+                        ) : (
+                                <Checkout {...props} baseUrl={this.baseUrl} />
+                            )
+                    )} />
+
+                    {/* Routing to the Profile page */}
+                    <Route path='/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl} />} />
+                </div>
+            </Router>
+        )
+    }
+}
+
+export default Controller;
